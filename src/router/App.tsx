38e05748710fdb8f092046223a84ea8547fc6623
cristianiniguez <felizpc1@gmail.com>
2021-11-05 13:16:42 -0400

    react router dom added

---
+++ src/router/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Calculator from '../pages/Calculator';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/calculator' element={<Calculator />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
